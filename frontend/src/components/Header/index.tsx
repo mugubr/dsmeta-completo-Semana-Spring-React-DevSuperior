7857cb1d3e1d612a3b32d4734b312d9ed07ec91e
mugubr <miguimuniz@gmail.com>
2022-07-15 14:30:31 -0300

    Configuração de segurança (Spring)

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
         <h1>DSMeta</h1>
         <p>
           Desenvolvido por
-          <a href="github.com/mugubr">@mugubr</a>
+          <a href="github.com/mugubr"> @mugubr</a>
         </p>
       </div>
     </header>
