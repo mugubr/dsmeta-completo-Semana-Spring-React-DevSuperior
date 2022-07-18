6aad774dd8f10d339abd6b4634eb67ea502bba5d
Miguel Muniz <miguimuniz@gmail.com>
2022-07-18 13:48:56 -0300

    Mais erros

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -10,10 +10,15 @@ type Props = {
   saleId: number;
 }
 
+function reload() {
+  window.location.reload
+}
+
 function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
       toast.info("SMS enviado com sucesso");
+      reload()
     });
 }
 
